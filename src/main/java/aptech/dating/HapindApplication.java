3684a9817ccb1c47697a18e122e689d0b018df93
win <win@Buivanbi>
2023-12-06 20:31:54 +0700

    roi do

--- src/main/java/aptech/dating/HapindApplication.java
+++ src/main/java/aptech/dating/HapindApplication.java
@@ -5,16 +5,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
 @SpringBootApplication
 public class HapindApplication {
 
     public static void main(String[] args) {
 	SpringApplication.run(HapindApplication.class, args);
     }
-//Test thu cai nha
+
     @Bean
     public ModelMapper getModelMapper() {
 	return new ModelMapper();
+    }
+    
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+        // Other ObjectMapper configurations can be added here
+        return objectMapper;
     }
 
 }
